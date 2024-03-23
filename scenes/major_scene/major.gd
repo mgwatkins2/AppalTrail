5f6f738dc2eb729654fe4233d174d5018adbe099
Mae Watkins <mgwatkins2@gmail.com>
2024-03-23 15:00:23 -0400

    Added Global Variables, Major screen Added a start and major screen to select your major Began working on framework for "global" variables such as weather, time, major, and energy.

---
+++ scenes/major_scene/major.gd
@@ -0,0 +1,38 @@
+extends Node2D
+var choice = 0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print(choice)
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ENTER:
+			print("Enter Key!")
+			if choice == 1:
+				# SET MAJOR TO BUSINESS
+				choice = 1
+			elif choice == 2:
+				# SET MAJOR TO CS
+				choice = 2
+			elif choice == 3:
+				# SET MAJOR TO ED
+				choice = 3
+			elif choice == 4:
+				# GO TO MAJOR DESCRIPTION SCENE
+				choice = 4
+		elif event.pressed and event.keycode == KEY_DELETE or event.keycode == KEY_BACKSPACE:
+			choice = 0;
+		elif event.pressed and event.keycode == KEY_1 && choice == 0:
+			choice = 1
+		elif event.pressed and event.keycode == KEY_2 && choice == 0:
+			choice = 2
+		elif event.pressed and event.keycode == KEY_3 && choice == 0:
+			choice = 3
+		elif event.pressed and event.keycode == KEY_4 && choice == 0:
+			choice = 4
---
+++ scenes/start_scene/start.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ENTER:
+			print("Enter Key!")
+			get_tree().change_scene_to_file("res://scenes/major_scene/major.tscn")
+		elif event.pressed and event.keycode == KEY_SPACE:
+			print("Space Key!")
+			get_tree().change_scene_to_file("res://scenes/major_scene/major.tscn")
